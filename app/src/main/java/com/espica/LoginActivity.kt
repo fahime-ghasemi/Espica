d6247aa34e2e8f92f5dd528ff8bee83f295ea4a2
Fahime Ghasemi <fahime.s.ghasemi@gmail.com>
2019-04-16 22:13:58 +0430

    Fix ui of login/sendcode/getPhont

--- app/src/main/java/com/espica/LoginActivity.kt
+++ app/src/main/java/com/espica/LoginActivity.kt
@@ -3,13 +3,15 @@ package com.espica
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentManager
+import com.espica.ui.SendCodeFragment
 
 class LoginActivity : AppCompatActivity() {
 
-    var fragmentManager : FragmentManager = supportFragmentManager
+    var fragmentManager: FragmentManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        fragmentManager.beginTransaction().replace(R.id.frameLayout, SendCodeFragment.newInstance())
     }
 }
